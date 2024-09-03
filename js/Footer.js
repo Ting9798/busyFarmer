ed958f37ea2479c56e90d6365e3c8516eca79178
Ting9798 <h821026888@gmail.com>
2024-09-04 04:12:40 +0800

    0903

---
+++ js/Footer.js
@@ -0,0 +1,55 @@
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div id="footer1">
+                    <div id="footer-content">
+                        <div className="footer-content-box">
+                            <div className="footer-contact">
+                                <div className="footer-contact-box">
+                                    <div className="media">
+                                        <a href="#"><img src="../images/shared/line.svg" alt="連接到line官方" /></a>
+                                        <a href="#"><img src="../images/shared/facebook.svg" alt="連接到facebook官方" /></a>
+                                        <a href="#"><img src="../images/shared/instagram.svg" alt="連接到instagram官方" /></a>
+                                    </div>
+
+                                    <address className="information">
+                                        <div>地址：台北市中正區農村路123號</div>
+                                        <div>電話：+886-2-1234-5678</div>
+                                        <div>電子郵件：info@farmersbusy.com</div>
+                                        <div>傳真：+886-2-1234-5679</div>
+                                    </address>
+                                </div>
+                                <div className="company"><img src="../images/shared/company-right.svg" alt="小農logo" /></div>
+                            </div>
+                            <div className="footer-map">
+                                <a href="#">首頁</a>
+                                <a href="#">公益理念</a>
+                                <a href="#">關於小農</a>
+                                <a href="#">小農商城</a>
+                                <a href="#">客製果乾</a>
+                                <a href="#">GO遊趣</a>
+                                <a href="#">小農精選</a>
+                            </div>
+                            <div className="copyright">©小農好忙有限公司</div>
+                        </div>
+                    </div>
+                    <div className="footerBg">
+                        <img className="footer-pre" src="../images/shared/footer-pre.png" alt="" />
+                        <img className="footer-back" src="../images/shared/footer-back.png" alt="" />
+                    </div>
+                </div>
+                <div id="footer2">
+                    <div className="copyright">
+                        <div>©小農好忙有限公司</div>
+                        <div className="media">
+                            <a href="#"><img src="../images/shared/line.svg" alt="連接到line官方" /></a>
+                            <a href="#"><img src="../images/shared/facebook.svg" alt="連接到facebook官方" /></a>
+                            <a href="#"><img src="../images/shared/instagram.svg" alt="連接到instagram官方" /></a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
