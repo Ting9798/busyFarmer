adab611c351ae6b5c63b2e6f196dcc97fd3bc3a6
Ting9798 <h821026888@gmail.com>
2024-09-10 22:23:51 +0800

    0910

--- js/FloatCart.js
+++ js/FloatCart.js
@@ -1,25 +1,32 @@
-const { useState } = React;
+const { useState, useEffect, forwardRef, useImperativeHandle } = React;
 
 
-function FloatCart() {
+const FloatCart = forwardRef((props, ref) => {
     const [isOpen, setIsOpen] = useState(false);
-    const [quantity, setQuantity] = useState(1);
     const [items, setItems] = useState([]);
 
-    // 讀取購物車
-    const getInitialItems = () => {
-        const savedItems = localStorage.getItem('cartItems');
-        return savedItems ? JSON.parse(savedItems) : [];
+    const [cartItems, setCartItems] = useState([]);
+
+    useEffect(() => {
+        // 讀取 localStorage 中的購物車資料
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+        setItems(cart);
+    }, []);
+
+    // 更新購物車的函數
+    const updateCart = (newItems) => {
+        setItems(newItems);
+        localStorage.setItem('cart', JSON.stringify(newItems));
     };
 
-    // 从 JSON 文件加载商品数据
+    // 從 JSON 文件加载商品
     useEffect(() => {
         const loadItems = async () => {
             try {
                 const response = await fetch('../public/item.json');
                 const data = await response.json();
                 const initialItems = getInitialItems();
-                // JSON 文件
+                // 更新商品数据
                 setItems(data.map(item => {
                     const savedItem = initialItems.find(i => i.id === item.id);
                     return savedItem ? savedItem : item;
@@ -35,6 +42,24 @@ function FloatCart() {
     useEffect(() => {
         localStorage.setItem('cartItems', JSON.stringify(items));
     }, [items]);
+
+    // 將值傳送
+    useImperativeHandle(ref, () => ({
+        addItemToCart: (newItem) => {
+            setItems(prevItems => {
+                const existingItem = prevItems.find(item => item.id === newItem.id);
+                if (existingItem) {
+                    return prevItems.map(item =>
+                        item.id === newItem.id
+                            ? { ...item, quantity: item.quantity + newItem.quantity }
+                            : item
+                    );
+                } else {
+                    return [...prevItems, newItem];
+                }
+            });
+        }
+    }));
 
     // 購物車收合
     const toggleCart = () => {
@@ -61,11 +86,17 @@ function FloatCart() {
 
     // 删除商品
     const handleDeleteItem = (itemId) => {
-        setItems(prevItems => prevItems.filter(item => item.id !== itemId));
+        // 從列表中移除商品
+        const updatedItems = items.filter(item => item.id !== itemId);
+        setItems(updatedItems);
+        
+        // 更新 localStorage
+        localStorage.setItem('cart', JSON.stringify(updatedItems));
     };
 
     // 計算總金額
     const totalPrice = items.reduce((total, item) => total + item.price * item.quantity, 0);
+
     return (
         <>
             <div className={`floating-cart ${isOpen ? 'open' : ''}`}>
@@ -95,22 +126,15 @@ function FloatCart() {
                                                     <div>{item.nameEg}</div>
                                                 </h2>
                                                 <button className="delItem" onClick={() => handleDeleteItem(item.id)}>
-                                                    <svg width="16" height="16" viewBox="0 0 8 8" fill="none"
-                                                        xmlns="http://www.w3.org/2000/svg">
+                                                    <svg width="16" height="16" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <g clipPath="url(#clip0_1915_6560)">
-                                                            <rect x="8" y="8" width="8" height="8" rx="4" transform="rotate(-180 8 8)"
-                                                                fill="#6C8C42" />
-                                                            <path
-                                                                d="M5.75874 2.65465C5.83908 2.57431 5.83908 2.44406 5.75874 2.36372C5.67841 2.28339 5.54816 2.28339 5.46782 2.36372L2.36463 5.46691C2.28429 5.54725 2.28429 5.6775 2.36463 5.75784C2.44497 5.83817 2.57522 5.83817 2.65555 5.75784L5.75874 2.65465Z"
-                                                                fill="#F1F1F1" />
-                                                            <path
-                                                                d="M5.46596 5.75777C5.54629 5.8381 5.67655 5.8381 5.75688 5.75777C5.83722 5.67743 5.83722 5.54718 5.75688 5.46684L2.65369 2.36365C2.57336 2.28332 2.44311 2.28332 2.36277 2.36365C2.28243 2.44399 2.28243 2.57424 2.36277 2.65458L5.46596 5.75777Z"
-                                                                fill="#F1F1F1" />
+                                                            <rect x="8" y="8" width="8" height="8" rx="4" transform="rotate(-180 8 8)" fill="#6C8C42" />
+                                                            <path d="M5.75874 2.65465C5.83908 2.57431 5.83908 2.44406 5.75874 2.36372C5.67841 2.28339 5.54816 2.28339 5.46782 2.36372L2.36463 5.46691C2.28429 5.54725 2.28429 5.6775 2.36463 5.75784C2.44497 5.83817 2.57522 5.83817 2.65555 5.75784L5.75874 2.65465Z" fill="#F1F1F1" />
+                                                            <path d="M5.46596 5.75777C5.54629 5.8381 5.67655 5.8381 5.75688 5.75777C5.83722 5.67743 5.83722 5.54718 5.75688 5.46684L2.65369 2.36365C2.57336 2.28332 2.44311 2.28332 2.36277 2.36365C2.28243 2.44399 2.28243 2.57424 2.36277 2.65458L5.46596 5.75777Z" fill="#F1F1F1" />
                                                         </g>
                                                         <defs>
                                                             <clipPath id="clip0_1915_6560">
-                                                                <rect x="8" y="8" width="8" height="8" rx="4" transform="rotate(-180 8 8)"
-                                                                    fill="white" />
+                                                                <rect x="8" y="8" width="8" height="8" rx="4" transform="rotate(-180 8 8)" fill="white" />
                                                             </clipPath>
                                                         </defs>
                                                     </svg>
@@ -120,12 +144,9 @@ function FloatCart() {
                                                 <div className="quantity-controls-price">NT.{item.price}</div>
                                                 <div className="quantity-controls-buttons">
                                                     <button className="decrement" onClick={() => handleDecrement(item.id)}>
-                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                                                            xmlns="http://www.w3.org/2000/svg">
+                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <rect width="16" height="16" rx="4" fill="#6C8C42" />
-                                                            <path
-                                                                d="M12.6577 8.73405C12.9017 8.73405 13.0996 8.53618 13.0996 8.2921C13.0996 8.04802 12.9017 7.85016 12.6577 7.85016L3.22949 7.85016C2.98541 7.85016 2.78755 8.04802 2.78755 8.2921C2.78755 8.53618 2.98541 8.73405 3.22949 8.73405L12.6577 8.73405Z"
-                                                                fill="#F1F1F1" />
+                                                            <path d="M12.6577 8.73405C12.9017 8.73405 13.0996 8.53618 13.0996 8.2921C13.0996 8.04802 12.9017 7.85016 12.6577 7.85016L3.22949 7.85016C2.98541 7.85016 2.78755 8.04802 2.78755 8.2921C2.78755 8.53618 2.98541 8.73405 3.22949 8.73405L12.6577 8.73405Z" fill="#F1F1F1" />
                                                         </svg>
                                                     </button>
 
@@ -133,19 +154,19 @@ function FloatCart() {
                                                     <input type="text" className="quantity-input" id={`quantity-input-${item.id}`} value={item.quantity} readOnly />
 
                                                     <button className="increment" onClick={() => handleIncrement(item.id)}>
-                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                                                            xmlns="http://www.w3.org/2000/svg">
+                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <rect width="16" height="16" rx="4" fill="#6C8C42" />
-                                                            <path
-                                                                d="M12.6577 8.73405C12.9017 8.73405 13.0996 8.53618 13.0996 8.2921C13.0996 8.04802 12.9017 7.85016 12.6577 7.85016L3.22949 7.85016C2.98541 7.85016 2.78755 8.04802 2.78755 8.2921C2.78755 8.53618 2.98541 8.73405 3.22949 8.73405L12.6577 8.73405Z"
-                                                                fill="#F1F1F1" />
-                                                            <path
-                                                                d="M7.50087 13.0062C7.50087 13.2503 7.69874 13.4481 7.94282 13.4481C8.1869 13.4481 8.38477 13.2503 8.38477 13.0062L8.38477 3.57801C8.38477 3.33393 8.1869 3.13607 7.94282 3.13607C7.69874 3.13607 7.50087 3.33393 7.50087 3.57801L7.50087 13.0062Z"
-                                                                fill="#F1F1F1" />
+                                                            <path d="M12.6577 8.73405C12.9017 8.73405 13.0996 8.53618 13.0996 8.2921C13.0996 8.04802 12.9017 7.85016 12.6577 7.85016L3.22949 7.85016C2.98541 7.85016 2.78755 8.04802 2.78755 8.2921C2.78755 8.53618 2.98541 8.73405 3.22949 8.73405L12.6577 8.73405Z" fill="#F1F1F1" />
+                                                            <path d="M7.50087 13.0062C7.50087 13.2503 7.69874 13.4481 7.94282 13.4481C8.1869 13.4481 8.38477 13.2503 8.38477 13.0062L8.38477 3.57801C8.38477 3.33393 8.1869 3.13607 7.94282 3.13607C7.69874 3.13607 7.50087 3.33393 7.50087 3.57801L7.50087 13.0062Z" fill="#F1F1F1" />
                                                         </svg>
                                                     </button>
                                                 </div>
                                             </div>
+                                            {item.notes && (
+                                                <div className="item-notes">
+                                                    <div>備註:</div> {item.notes}
+                                                </div>
+                                            )}
                                         </div>
                                     </li>
                                 ))}
@@ -162,4 +183,4 @@ function FloatCart() {
             </div>
         </>
     );
-}
+});
