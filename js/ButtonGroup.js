42a654ae9ad3a199bf30780e9b909deaa97bdb2f
Ting9798 <h821026888@gmail.com>
2024-09-24 14:45:38 +0800

    0924

--- js/ButtonGroup.js
+++ js/ButtonGroup.js
@@ -10,12 +10,10 @@ function ButtonGroup(props) {
         onClick,
         cart,
         shouldCheckCart = false,
-        isButton = false // 新增屬性
     } = props;
 
     const handleClick = (e) => {
         if (shouldCheckCart && cart.length === 0) {
-            // 如果購物車是空的
             e.preventDefault(); // 阻止下一頁
             alert('請添加物品進入購物車！');
         } else if (onClick) {
@@ -23,16 +21,14 @@ function ButtonGroup(props) {
         }
     };
 
-    const Wrapper = isButton ? 'button' : 'div'; // 根據 isButton 判斷包裝器
-
     return (
-        <di className={`button-group ${btnPosition || ''}`} onClick={handleClick}>
-            <Wrapper className={`button-group-box ${btnPosition || ''}`}>
-                {preBtnClass &&
-                    (<a href={preBtnSrc || ''} className={`button ${preBtnClass || ''}`}>{preBtnName || ''}</a>)
-                }
+        <div className={`button-group ${btnPosition || ''}`} onClick={handleClick}>
+            <div className={`button-group-box ${btnPosition || ''}`}>
+                {preBtnClass && (
+                    <a href={preBtnSrc || ''} className={`button ${preBtnClass || ''}`}>{preBtnName || ''}</a>
+                )}
                 <a href={nextBtnSrc || ''} className={`button ${nextBtnClass || ''}`}>{nextbtnName}</a>
-            </Wrapper>
-        </di>
+            </div>
+        </div>
     );
 }
